e532e1d954d2ad850bcfa970216bd0b771e57789
Jihun_s <kdne0610@gmail.com>
2023-08-11 18:54:32 +0900

    First_Commit_Jihun

---
+++ src/test/java/net/softsociety/secretary/SecretaryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.softsociety.secretary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
