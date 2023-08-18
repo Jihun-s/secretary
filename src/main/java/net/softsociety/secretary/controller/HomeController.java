4b5611422356ab5e2182da4b5051ef8c91d42b08
Jihun_Kim <kdne0610@naver.com>
2023-08-18 17:13:45 +0900

    Thymeleaf Clear! - Jihun_Commit

--- src/main/java/net/softsociety/secretary/controller/HomeController.java
+++ src/main/java/net/softsociety/secretary/controller/HomeController.java
@@ -1,15 +1,8 @@
 package net.softsociety.secretary.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Controller
 public class HomeController {
 
@@ -17,9 +10,8 @@ public class HomeController {
 	 * 메인화면!
 	 */
 	@GetMapping({"", "/"})
-    public void redirectToNewUrl(HttpServletResponse response) throws IOException {
-        response.sendRedirect("/secretary/html/index.html");
-    }
-	
-	
+	public String home() {
+		return "index";
+	}
+
 }
---
+++ src/main/java/net/softsociety/secretary/controller/TestLinkController.java
@@ -0,0 +1,26 @@
+package net.softsociety.secretary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestLinkController {
+	
+    //OCR테스트
+    @GetMapping("/ocrtest")
+    public String ocrTest() {
+        return "testLinkView/ocrTest";
+    }
+
+    //Voice테스트
+    @GetMapping("/speechtest")
+    public String speechTest() {
+        return "testLinkView/speechTest";
+    }
+    
+    //Login테스트
+    @GetMapping("/auth-login-basic")
+    public String authLoginBasic() {
+        return "auth-login-basic";
+    }
+}
