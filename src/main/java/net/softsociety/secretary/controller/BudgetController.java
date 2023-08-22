cf7fe362217ddf08815244c68570dec0239731e4
soyo <soyolee.dev@gmail.com>
2023-08-22 14:12:37 +0900

    소영 옷장 템플릿 틀 html

---
+++ src/main/java/net/softsociety/secretary/controller/BudgetController.java
@@ -0,0 +1,15 @@
+package net.softsociety.secretary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("budget")
+public class BudgetController {
+
+
+	
+}
