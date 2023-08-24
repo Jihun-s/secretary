596e7c03e4ba6037240dc09e4d1497037fc6f020
soyo <soyolee.dev@gmail.com>
2023-08-24 17:07:54 +0900

    소영 가계부 - 컨트롤러 쪼개기(Page/Budget/Rest)

--- src/main/java/net/softsociety/secretary/controller/CashbookBudgetController.java
+++ src/main/java/net/softsociety/secretary/controller/CashbookBudgetController.java
@@ -3,17 +3,22 @@ package net.softsociety.secretary.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
+
+/** 예산 컨트롤러 */
 
 @Slf4j
 @Controller
 @RequestMapping("cashbook")
-@ResponseBody
-public class CashbookRestController {
-	
-	
+public class CashbookBudgetController {
 
+	/** 예산 설정 */
+	@PostMapping("setBudget")
+	public String setBudget(int budgetAmount) {
+		log.debug("넘어온 예산:{}", budgetAmount);
+	
+		return "redirect:/cashbook/";
+	}
 	
 }
