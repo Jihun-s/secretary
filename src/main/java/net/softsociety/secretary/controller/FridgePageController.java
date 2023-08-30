6ef91f411ac25369c014b4dd7de6db3c597ac3ad
Jihun_Kim <kdne0610@naver.com>
2023-08-30 17:52:48 +0900

    냉장고 추가 기능 구현 - Jihun

--- src/main/java/net/softsociety/secretary/controller/FridgePageController.java
+++ src/main/java/net/softsociety/secretary/controller/FridgePageController.java
@@ -1,16 +1,37 @@
 package net.softsociety.secretary.controller;
 
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
+import net.softsociety.secretary.domain.Fridge;
+import net.softsociety.secretary.domain.User;
+import net.softsociety.secretary.service.FridgeService;
+import net.softsociety.secretary.service.UserService;
 
 @Slf4j
 @Controller
-@RequestMapping("fridge")
+@RequestMapping("/fridge")
 public class FridgePageController {
 
+	@Autowired
+	private FridgeService fridgeService;
+	
+    @Autowired
+    private UserService userService;
+	
 	// 냉장고 메인화면
 	@GetMapping({"", "/"})
 	public String fridgeMain() {
@@ -31,5 +52,46 @@ public class FridgePageController {
 		
 		return "fridgeView/fridge2";
 	}
-	
+
+	@ResponseBody
+    @GetMapping("getAllFridges")
+    public List<Fridge> getAllFridges(Principal principal) {
+        // Principal 객체를 사용하여 현재 인증된 사용자의 이름을 가져옵니다.
+        String username = principal.getName();
+
+        // UserService를 사용하여 User 객체를 검색합니다.
+        User user = userService.findByEmailOrUserId(username);
+
+        // User 객체에서 family_id를 가져옵니다.
+        int familyId = user.getFamilyId();
+        return fridgeService.getAllFridges(familyId);
+    }
+
+	@ResponseBody
+    @PostMapping("addFridge")
+    public void addFridge(@RequestBody Fridge fridge, Principal principal) {
+	    // Principal 객체를 사용하여 현재 인증된 사용자의 이름을 가져옵니다.
+	    String username = principal.getName();
+
+	    // UserService를 사용하여 User 객체를 검색합니다.
+	    User user = userService.findByEmailOrUserId(username);
+
+	    // User 객체에서 family_id를 가져옵니다.
+	    int familyId = user.getFamilyId();
+
+	    // Fridge 객체에 family_id를 설정합니다.
+	    fridge.setFamilyId(familyId);
+		
+        fridgeService.addFridge(fridge);
+    }
+
+    @PutMapping("/{id}")
+    public Fridge updateFridge(@PathVariable int id, @RequestBody Fridge fridge) {
+        return fridgeService.updateFridge(id, fridge);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFridge(@PathVariable int id) {
+        fridgeService.deleteFridge(id);
+    }
 }
---
+++ src/main/java/net/softsociety/secretary/service/FridgeService.java
@@ -0,0 +1,12 @@
+package net.softsociety.secretary.service;
+
+import java.util.List;
+
+import net.softsociety.secretary.domain.Fridge;
+
+public interface FridgeService {
+    List<Fridge> getAllFridges(int familyId);
+    void addFridge(Fridge fridge);
+    Fridge updateFridge(int id, Fridge fridge);
+    void deleteFridge(int id);
+}
