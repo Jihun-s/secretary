84a5ffeebf81dd846001711568728bc61be5e3b3
Jihun_Kim <kdne0610@naver.com>
2023-08-31 21:25:43 +0900

    이미지, 보이스 입력 화면 구성

---
+++ src/main/java/net/softsociety/secretary/domain/FridgeFood.java
@@ -0,0 +1,25 @@
+package net.softsociety.secretary.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FridgeFood {
+
+    private int foodId;
+    private String foodName;
+    private int fridgeId;
+    private int foodAmount;
+    private String foodCategory;
+    private Date foodInputDate;
+    private Date foodMadeDate;
+    private Date foodExpiryDate;
+    private Long foodPrice;
+    private String foodOriginalFile;
+    private String foodSavedFile;
+}
