227891eb9a55eee97098afa38f00920481cda7e0
soyo <soyolee.dev@gmail.com>
2023-10-01 17:25:53 +0900

    소영 가계부 - 주별요약 데이터 없을 시 대체 텍스트 적용

--- src/main/resources/static/js/cashbook/cashbookChart.js
+++ src/main/resources/static/js/cashbook/cashbookChart.js
@@ -376,22 +376,34 @@ function weekExpenseAcc() {
     data: { chYear: curYear, chMonth: curMonth },
     dataType: 'JSON',
     success: (data) => {
-      // console.log("주별 요약:" + JSON.stringify(data));
-      // [{"familyId":1,"userId":null,"cate1Name":null,"cate2Name":null,"labelColor":null,"curYear":2023,"curMonth":9,"weekOfMonth":1,"totalMonthExpense":null,"totalMonthIncome":null,"totalWeekExpense":780200,"weekAccumulatedExpense":780200}]
-
-      let htmlToInsert = '';
-      data.forEach((wea) => {
-      htmlToInsert += `
-          <p class="list-group-item list-group-item-action d-flex justify-content-between">
-            <span>${wea.weekOfMonth}주차</span>
-            <span>총 ${wea.weekAccumulatedExpense.toLocaleString('en-US')}원</span>
-            <small>+${wea.totalWeekExpense.toLocaleString('en-US')}</small>
-          </p>
+      // Check if data is empty
+      if (!data || data.length === 0) {
+        const noExDataHTML = `
+          <img src="https://cdn2.iconfinder.com/data/icons/business-1538/512/icbs01_1.png" alt="noCashbookData" style="width: 15rem; height: 15rem;" />
+          <div class="mt-3 mb-3">
+              <p>이번 달 지출 거래내역이 존재하지 않습니다.</p>
+              <p>내역을 입력하러 가볼까요?</p>
+              <a href="/secretary/cashbook/trans">
+                  <button type="button" class="btn btn-success">
+                      가계부 내역 바로가기
+                  </button>
+              </a>
+          </div>    
         `;
-      });
-
-      $('#weekExpenseAccDiv').html(htmlToInsert);
-
+        $('#weekExpenseAccDiv').html(noExDataHTML);
+      } else {
+        let htmlToInsert = '';
+        data.forEach((wea) => {
+          htmlToInsert += `
+            <p class="list-group-item list-group-item-action d-flex justify-content-between">
+              <span>${wea.weekOfMonth}주차</span>
+              <span>총 ${wea.weekAccumulatedExpense.toLocaleString('en-US')}원</span>
+              <small>+${wea.totalWeekExpense.toLocaleString('en-US')}</small>
+            </p>
+          `;
+        });
+        $('#weekExpenseAccDiv').html(htmlToInsert);
+      }
     },
     error: (e) => {
       alert("주별 요약 전송 실패");
@@ -399,7 +411,6 @@ function weekExpenseAcc() {
     }
   });
 }
-
 
 
 /////주별요약/////주별요약/////주별요약/////주별요약/////주별요약/////주별요약/////주별요약/////주별요약/////주별요약/////주별요약/////주별요약/////
