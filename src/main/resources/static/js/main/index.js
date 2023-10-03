e8b357f3593198ff9fcb24d468fbe1c0c9953832
soyo <soyolee.dev@gmail.com>
2023-10-03 18:05:12 +0900

    소영 알림 - 종 아이콘, 사이드바 '알림' 메뉴 클릭 시 알림 전용 모달 띄우도록 수정 (가계부알림에 deleteAlert는 구현안됨)

--- src/main/resources/static/js/main/index.js
+++ src/main/resources/static/js/main/index.js
@@ -1,3 +1,428 @@
+/** 예산 설정 여부 확인하는 전역변수 */
+let budgetExist = 0;
+/** (예산-지출) > 0인지 확인하는 전역변수 */
+let budgetMinus = 0;
+
+
+/** 필수알림 가져오는 함수 */
+function getPilsuAlert() {
+let now = new Date(); 
+let curYear = now.getFullYear();
+let curMonth = now.getMonth() + 1;
+let curDate = now.getDate();
+
+/** 예산 있없? 확인 */
+$.ajax({
+    url: '/secretary/cashbook/budgetExist',
+    type: 'GET',
+    data: { curYear: curYear, curMonth: curMonth },
+    dataType: 'text',
+    success: (result) => {
+        if(result == 1) {
+            budgetExist = 1;
+
+            // 예산 초과했는지 확인
+            $.ajax({
+                url: '/secretary/cashbook/budget/getBudgetRest',
+                type: 'GET',
+                data: { curYear: curYear, curMonth: curMonth },
+                success: (result) => {
+                if (result <= 0) {
+                    budgetMinus = 1;
+                    // alert("예산초과?:" + budgetMinus);
+                }
+                },
+                error: (e) => {
+                alert('남은 예산 서버 전송 실패');
+                console.log(JSON.stringify(e));
+                }
+            });
+
+        }
+    },
+    error: (e) => {
+        alert('알림 출력 직전 예산 있없 조회 실패');
+        console.log(JSON.stringify(e));
+    }
+});
+
+/** 필수알림 목록 가져오는 ajax */
+$.ajax({
+    url: '/secretary/cashbook/alert/getPilsuAlert',
+    type: 'POST',
+    data: { curDateTime: curDateTime, curYear: curYear, curMonth: curMonth, curDate: curDate },
+    dataType: 'JSON',
+    success: function(data) {
+    // console.log("필수알림 data: " + JSON.stringify(data));
+
+    let html = "";
+
+    if (!data || data.length === 0) {
+    html = `<p>표시할 필수 알림이 없습니다.</p>`;
+    $('#pilsuAlertListDiv').html(html);
+    return;
+    }
+
+    // 예산 없을 때 알림
+    if(budgetExist == 0) {
+    html += `<br><small class="text-light fw-semibold mb-2">${curYear}-${curMonth}-${curDate}</small>`;
+    html += `
+        <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" style="border: none;">
+            <a href="/secretary/cashbook">
+                <div>
+                ${curMonth}월 예산이 설정되지 않았습니다. 여기를 클릭하면 예산을 설정할 수 있어요.
+                </div>
+            </a>
+        </div>
+        `;
+    }
+        
+    // 예산 초과했을 때 알림
+    if(budgetMinus == 1) {
+    // alert("알림 추가하자!");
+    html += `<br><small class="text-light fw-semibold mb-2">${curYear}-${curMonth}-${curDate}</small>`;
+    html += `
+        <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" style="border: none;">
+            <a href="javascript:openUpdateBudgetModal()">
+            <div>
+            ${curMonth}월 예산을 초과했어요! 과소비를 계속하면 길바닥에 나앉을 수 있어요. 여기를 눌러 예산을 다시 설정할 수 있어요.
+            </div>
+            </a>
+        </div>
+    `;
+    // alert("알림 추가 완료!");
+    }
+
+
+    // 알림을 alertDateYmd 기준으로 그룹화
+    let groupedByDate = {};
+    data.forEach(alert => {
+    if (!groupedByDate[alert.alertDateYmd]) {
+        groupedByDate[alert.alertDateYmd] = [];
+    }
+    groupedByDate[alert.alertDateYmd].push(alert);
+    });
+
+    // 객체의 키를 추출해 내림차순으로 정렬
+    let sortedDates = Object.keys(groupedByDate).sort((a, b) => b.localeCompare(a));
+
+    // 내림차순한 배열 
+    let sortedGroupedByDate = {};
+    sortedDates.forEach(date => {
+    sortedGroupedByDate[date] = groupedByDate[date];
+    });
+
+
+    // console.log("제안 sortedGroupedByDate: " + JSON.stringify(sortedGroupedByDate));
+
+    // 그룹별 키워드
+
+    // 총수입 총지출
+    let inexTotal = ["지난달"];
+
+    // 지출
+    let exWeek = ["지난주"];
+    
+    // 예산
+    // 정기소득
+    let bgRest = ["남은예산"];
+
+    // 그룹화된 데이터를 기반으로 HTML 생성
+    for (let date in sortedGroupedByDate) {
+    html += `<br><small class="text-light fw-semibold mb-2">${date}</small>`;
+    // 가져온 데이터 분기
+    sortedGroupedByDate[date].forEach(alert => {
+        html += `
+        <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" style="border: none;">
+        <a href="javascript:openDetailModal(${alert.alertId});">
+        <div>
+        `;
+
+        // 지난달총수입총지출
+        if (inexTotal.some(keyword => alert.alertContent.includes(keyword))) {
+            html += `
+            지난 달에는 ${alert.totalIncomeMonth.toLocaleString('en-US')}원을 벌고 ${alert.totalExpenseMonth.toLocaleString('en-US')}원을 지출했어요.
+        `;
+            // 수입 < 지출
+            if(alert.totalIncomeMonth < alert.totalExpenseMonth) {
+            html += `배보다 배꼽이 더 큰 한 달이네요. 🤯`;
+            }
+            // 수입 = 지출
+            else if (alert.totalIncomeMonth == alert.totalExpenseMonth) {
+            html += `버는 족족 써버리고 말았네요. 💸`;
+            }
+            // 수입 > 지출
+            else if (alert.totalIncomeMonth == alert.totalExpenseMonth) {
+            html += `저축이나 재테크의 비중을 높여도 좋겠어요.`;
+            }
+        }
+        // 지난주총지출
+        else if (exWeek.some(keyword => alert.alertContent.includes(keyword))) {
+            html += `
+            지난 주 총 지출은 ${alert.totalWeekExpense.toLocaleString('en-US')}원입니다.
+            `;
+        } 
+        // 남은예산
+        else if (bgRest.some(keyword => alert.alertContent.includes(keyword))) {
+            html += `
+            
+            `;
+            if(alert.budgetRest <= 0) {
+            html += `예산보다 ${alert.budgetRest.toLocaleString('en-US') * -1}원 더 지출했어요. 와! 길바닥에 나앉기 직전이에요. 🫵`;
+            }
+            else {
+            html += `이번 달 남은 예산은 ${alert.budgetRest.toLocaleString('en-US')}원입니다. 이번 주도 알뜰살뜰 노력해봐요. ☺️`;
+            }
+        } 
+        // 기타
+        else {
+            html += `
+            ${alert.alertDateMonth}월 ${alert.alertDateDay}일은 ${alert.alertContent}입니다.
+            `;
+        }
+
+        html += `
+        </div>
+            </a>
+            <i class="bx bx-x" style="cursor: pointer;" onclick="deleteAlert(${alert.alertId});"></i>
+        </div>
+        `; 
+
+        // console.log("이 알림의 번호는 " + alert.alertId);
+        });
+
+    }
+
+    $('#pilsuAlertListDiv').html(html);
+
+
+    },
+    error: (e) => {
+        alert('가계부 필수알림 목록 전송 실패');
+    }
+});
+}
+
+
+/** 공용 모달 알림 가져오는 함수 */
+function getPilsuAlertModal() {
+  let now = new Date();
+  let curYear = now.getFullYear();
+  let curMonth = now.getMonth() + 1;
+  let curDate = now.getDate();
+
+  /** 예산 있없? 확인 */
+  $.ajax({
+    url: "/secretary/cashbook/budgetExist",
+    type: "GET",
+    data: { curYear: curYear, curMonth: curMonth },
+    dataType: "text",
+    success: (result) => {
+      if (result == 1) {
+        budgetExist = 1;
+
+        // 예산 초과했는지 확인
+        $.ajax({
+          url: "/secretary/cashbook/budget/getBudgetRest",
+          type: "GET",
+          data: { curYear: curYear, curMonth: curMonth },
+          success: (result) => {
+            if (result <= 0) {
+              budgetMinus = 1;
+              // alert("예산초과?:" + budgetMinus);
+            }
+          },
+          error: (e) => {
+            alert("남은 예산 서버 전송 실패");
+            console.log(JSON.stringify(e));
+          },
+        });
+      }
+    },
+    error: (e) => {
+      alert("알림 출력 직전 예산 있없 조회 실패");
+      console.log(JSON.stringify(e));
+    },
+  });
+
+  /** 필수알림 목록 가져오는 ajax */
+  $.ajax({
+    url: "/secretary/cashbook/alert/getPilsuAlert",
+    type: "POST",
+    data: {
+      curDateTime: curDateTime,
+      curYear: curYear,
+      curMonth: curMonth,
+      curDate: curDate,
+    },
+    dataType: "JSON",
+    success: function (data) {
+      // console.log("필수알림 data: " + JSON.stringify(data));
+
+      let html = "";
+
+      if (!data || data.length === 0) {
+        html = `<p>표시할 필수 알림이 없습니다.</p>`;
+        $("#pilsuAlertListDivModal").html(html);
+        return;
+      }
+
+      // 예산 없을 때 알림
+      if (budgetExist == 0) {
+        html += `<br><small class="text-light fw-semibold mb-2">${curYear}-${curMonth}-${curDate}</small>`;
+        html += `
+          <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" style="border: none;">
+              <a href="/secretary/cashbook">
+                <div>
+                  ${curMonth}월 예산이 설정되지 않았습니다. 예산을 설정하고 돈을 효율적으로 관리해보세요.
+                </div>
+              </a>
+          </div>
+          `;
+      }
+
+      // 예산 초과했을 때 알림
+      if (budgetMinus == 1) {
+        // alert("알림 추가하자!");
+        html += `<br><small class="text-light fw-semibold mb-2">${curYear}-${curMonth}-${curDate}</small>`;
+        html += `
+          <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" style="border: none;">
+            <a href="javascript:openUpdateBudgetModal()">
+            <div>
+              ${curMonth}월 예산을 초과했어요! 현실적인 예산을 다시 설정하거나 반성하는 시간을 가져요.
+            </div>
+            </a>
+          </div>
+      `;
+        // alert("알림 추가 완료!");
+      }
+
+      // 알림을 alertDateYmd 기준으로 그룹화
+      let groupedByDate = {};
+      data.forEach((alert) => {
+        if (!groupedByDate[alert.alertDateYmd]) {
+          groupedByDate[alert.alertDateYmd] = [];
+        }
+        groupedByDate[alert.alertDateYmd].push(alert);
+      });
+
+      // 객체의 키를 추출해 내림차순으로 정렬
+      let sortedDates = Object.keys(groupedByDate).sort((a, b) =>
+        b.localeCompare(a)
+      );
+
+      // 내림차순한 배열
+      let sortedGroupedByDate = {};
+      sortedDates.forEach((date) => {
+        sortedGroupedByDate[date] = groupedByDate[date];
+      });
+
+      // console.log("제안 sortedGroupedByDate: " + JSON.stringify(sortedGroupedByDate));
+
+      // 그룹별 키워드
+
+      // 총수입 총지출
+      let inexTotal = ["지난달"];
+
+      // 지출
+      let exWeek = ["지난주"];
+
+      // 예산
+      // 정기소득
+      let bgRest = ["남은예산"];
+
+      // 그룹화된 데이터를 기반으로 HTML 생성
+      for (let date in sortedGroupedByDate) {
+        html += `<br><small class="text-light fw-semibold mb-2">${date}</small>`;
+        // 가져온 데이터 분기
+        sortedGroupedByDate[date].forEach((alert) => {
+          html += `
+        <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" style="border: none;">
+        <a href="javascript:openDetailModal(${alert.alertId});">
+          <div>
+        `;
+
+          // 지난달총수입총지출
+          if (
+            inexTotal.some((keyword) =>
+              alert.alertContent.includes(keyword)
+            )
+          ) {
+            html += `
+            지난 달에는 ${alert.totalIncomeMonth.toLocaleString(
+              "en-US"
+            )}원을 벌고 ${alert.totalExpenseMonth.toLocaleString(
+              "en-US"
+            )}원을 지출했어요.
+          `;
+            // 수입 < 지출
+            if (alert.totalIncomeMonth < alert.totalExpenseMonth) {
+              html += `배보다 배꼽이 더 큰 한 달이네요. 🤯`;
+            }
+            // 수입 = 지출
+            else if (alert.totalIncomeMonth == alert.totalExpenseMonth) {
+              html += `버는 족족 써버리고 말았네요. 💸`;
+            }
+            // 수입 > 지출
+            else if (alert.totalIncomeMonth == alert.totalExpenseMonth) {
+              html += `저축이나 재테크의 비중을 높여도 좋겠어요.`;
+            }
+          }
+          // 지난주총지출
+          else if (
+            exWeek.some((keyword) => alert.alertContent.includes(keyword))
+          ) {
+            html += `
+              지난 주 총 지출은 ${alert.totalWeekExpense.toLocaleString(
+                "en-US"
+              )}원입니다.
+            `;
+          }
+          // 남은예산
+          else if (
+            bgRest.some((keyword) => alert.alertContent.includes(keyword))
+          ) {
+            html += `
+            
+            `;
+            if (alert.budgetRest <= 0) {
+              html += `예산보다 ${
+                alert.budgetRest.toLocaleString("en-US") * -1
+              }원 더 지출했어요. 와! 길바닥에 나앉기 직전이에요. 🫵`;
+            } else {
+              html += `이번 달 남은 예산은 ${alert.budgetRest.toLocaleString(
+                "en-US"
+              )}원입니다. 이번 주도 알뜰살뜰 노력해봐요. ☺️`;
+            }
+          }
+          // 기타
+          else {
+            html += `
+            ${alert.alertDateMonth}월 ${alert.alertDateDay}일은 ${alert.alertContent}입니다.
+            `;
+          }
+
+          html += `
+          </div>
+            </a>
+            <i class="bx bx-x" style="cursor: pointer;" onclick="deleteAlert(${alert.alertId});"></i>
+          </div>
+          `;
+
+          // console.log("이 알림의 번호는 " + alert.alertId);
+        });
+      }
+
+      $("#pilsuAlertListDivModal").html(html);
+    },
+    error: (e) => {
+      alert("가계부 필수알림 목록 전송 실패");
+    },
+  });
+}
+
+
+
 /** 월 총 지출 불러오기 */
 function sumExpenseMonth() {
     nowYear = new Date().getFullYear();
